ad001c8aecd5abf417e975b2f06ef9927edce0ce
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-16 16:17:04 +0530

    Logger corrected

--- src/network_security/exception/exception.py
+++ src/network_security/exception/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.network_security.logging import logger
+import logger
 class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message=error_message
