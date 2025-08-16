17fc479552f035cc08dae824767939fc201f87ff
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-16 11:18:56 +0530

    Initial Folder structure added

---
+++ template.py
@@ -0,0 +1,28 @@
+from  pathlib import Path
+import logging, os
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
+project_name="network-security"
+list_of_file=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/cloud/__init__.py",
+    "params.yaml",
+    "config.yaml",
+    "Dockerfile",
+    ".dockerignore",
+    ".gitignore"
+]
+for filepath in list_of_file:
+    filepath=Path(filepath)
+    filedir,file_name=os.path.split(filepath)
+    if filedir!="":
+          os.makedirs(filedir,exist_ok=True)
+          logging.info(f"Creating empty file {file_name}")
+    if not os.path.exists(filepath) or (os.path.getsize(filepath))==0:
+         with open(filepath,"w") as f:
+              logging.info(f"Creating empty file {filepath}")
+    else:
+         logging.info(f"{file_name} already exists")
