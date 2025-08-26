8e7fd04030bb377f6d1c57529a49d52c319d6ef2
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-26 23:42:21 +0530

    Data ingestion initiated

--- network_security/constants/data_ingestion.py
+++ network_security/constants/data_ingestion.py
@@ -4,3 +4,13 @@ from network_security.logging.logger import logging
 from network_security.entity.config_entity import DataIngestionConfig
 import os,sys, pymongo,numpy as np
 from sklearn.model_selection import train_test_split
+from dotenv import load_dotenv 
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URI")
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            pass
+        except Exception as e:
+            raise e
