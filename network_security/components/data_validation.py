5ff8132a6c122cf943ceb9b5b066c4177d270072
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-27 02:26:17 +0530

    Validation Artifact added

--- network_security/components/data_validation.py
+++ network_security/components/data_validation.py
@@ -1,1 +1,10 @@
 # Data Drifting should not be there so we need to ensure data validation
+from network_security.entity.config_entity import DataValidationConfig
+from network_security.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+from scipy.stats import ks_2samp
+import pandas as pd, os, sys
+class DataValidation:
+    def __init__(self):
+        pass
