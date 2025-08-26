c1871fa319e4f8ca74be3045462191e5977a70b3
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-26 23:54:22 +0530

    Data ingestion initiated

--- network_security/constants/data_ingestion.py
+++ network_security/constants/data_ingestion.py
@@ -41,6 +41,6 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             dataframe=self.export_collection_as_dataframe()
-
+            dataframe=self.export_data_into_feature_store(dataframe)
         except Exception as e:
             raise NetworkSecurityException
