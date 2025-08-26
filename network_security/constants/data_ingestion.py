e098b95eca771fb74e2be9dcd857bf35985fdaa5
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-26 23:39:58 +0530

    Data ingestion initiated

---
+++ network_security/constants/data_ingestion.py
@@ -0,0 +1,5 @@
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+
+from network_security.entity.config_entity import DataIngestionConfig
+import os,sys
