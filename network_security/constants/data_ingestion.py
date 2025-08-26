7b83fc39095d42aba65ab5ddcb6f596be661a399
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-26 23:40:30 +0530

    Data ingestion initiated

--- network_security/constants/data_ingestion.py
+++ network_security/constants/data_ingestion.py
@@ -2,4 +2,5 @@ from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
 
 from network_security.entity.config_entity import DataIngestionConfig
-import os,sys
+import os,sys, pymongo,numpy as np
+from sklearn.model_selection import train_test_split
