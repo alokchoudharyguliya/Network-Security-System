f619f0f95a0400238d2d0db2165a1e30cf364d58
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-27 10:11:32 +0530

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -16,10 +17,16 @@ if __name__=="__main__":
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(training_pipeline_config=trainingpipelineconfig)
+        datavalidationconfig=DataValidationConfig(training_pipeline_config=trainingpipelineconfig)
         dataingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the Data Ingestion")
         dataingestionartifact=dataingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        logging.info("Data Ingestion Done")
+        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
+        logging.info("Initiate the Data Validation")
+        datavalidationartifact=data_validation.initiate_data_validation()
+        logging.info("Data Ingestion Done")
+        print(datavalidationartifact)
         # a=1/0
         # print("This will not be printed")
     except Exception as e:
